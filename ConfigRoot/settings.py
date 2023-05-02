96ba5986399a135d5514ac04db042af3bda8a48d
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-02 17:16:34 +0200

    added dockerizing

--- ConfigRoot/settings.py
+++ ConfigRoot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import environ
@@ -33,6 +33,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Take environment variables from .env file
 environ.Env.read_env(BASE_DIR / '.env')
 
+ENV = env("ENV")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -40,9 +42,9 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False if ENV == "PROD" else True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOST", "*").split(",")
 
 DOMAIN_NAME = env('DOMAIN_NAME')
 
